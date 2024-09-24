d0558b693da666b6f7d7e9b716b950df4d73963d
MajorFoxs_OvO <mckekejun@gmail.com>
2024-09-24 14:08:43 +0800

    Updata

---
+++ mmitemsmod/src/main/java/com/awafuns/mmitemsmod/MMitemsModClient.java
@@ -0,0 +1,10 @@
+package com.awafuns.mmitemsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MMitemsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
